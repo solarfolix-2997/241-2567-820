b23215cf52f159d574e371c568650bddf051df1e
solarFolix-2997 <Pattaradanai.bu@ku.th>
2025-03-03 17:15:46 +0700

    Update SQL

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 const port = 8000;
 
 let users = []
-// let counter = 1;  [ -- NO LONGER USED -- ]
 let conn = null
 
- const initMySQL = async () => {
+// sent user info in update to where it belongs
+/* 
+GET / USERS = get all users
+POST / USERS = create new user in data
+GET /users/:id = get user by id
+PUT /users/:id = get user by id
+DELETE / user/:id
+*/
+
+const initMySQL = async () => {
     conn = await mysql.createConnection({
         host:'localhost',
         user:     'root',  
@@ -18,25 +28,26 @@ let conn = null
         database: 'webdb',
         port: 8830
     })
- }
+}
 
- app.get('/testdb-new', async (req, res) => {
+app.get('/testdb-new', async (req, res) => {
     try{     
         const results = await conn.query('SELECT * FROM users')
-         res.json(results[0])    
+        res.json(results[0])    
     } catch(error){     
         console.log('Error fetching users:', error.message)
         res.status(500).json({error: 'Error fetching users'})
     }
- })
- 
-// Path = / GET / Users
+})
+
+// Path = GET / Users
 app.get('/users', async (req,res) => {
- // res.json(users); [ -- NO LONGER USED -- ]
+// res.json(users); [ -- NO LONGER USED -- ]
     const result = await conn.query('SELECT * FROM users')
     res.json(result[0])
 })
 
+/*
 // path = POST / User
 app.post('/user', async (req,res) => {
     let user = req.body;
@@ -47,52 +58,85 @@ app.post('/user', async (req,res) => {
         data: results
     });
 })
+*/
 
-// path = PUT / user/:id
-app.put('/user/:id', (req,res) => {
-    let id = req.params.id;
-    let updateUser = req.body;
-    // find users from id request
-    let selectedIndex = users.findIndex(user => user.id == id)
-    // update user 
-    if (updateUser.firstname){
-        users[selectedIndex].firstname = updateUser.firstname
-    }
-    if (updateUser.lastname){
-        users[selectedIndex].lastname = updateUser.lastname
-    }
-
-    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
-    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
-
+// path = POST / User
+app.post('/users', async (req,res) => {
+    try{
+    let user = req.body;
+    const result = await conn.query('INSERT INTO users SET ?', user)
     res.json({
-        message: 'User updated successfully',
+        message: 'User created successfully',
         data: {
-            user: updateUser,
-            indexUpdate: selectedIndex
+            message: 'User created',
+            data: result[0]
         }
     });
-    // sent user info in update to where it belongs
-    /* 
-    GET / USERS = get all users
-    POST / USERS = create new user in data
-    GET /users/:id = get user by id
-    PUT /users/:id = get user by id
-    */
- })
+    } catch (error){
+        res.status(500).json({
+            message: "Something went wrong",
+            errorMessage: error.message
+        })
+    }
+})
+
+// path = PUT / user/:id
+app.put('/users/:id', async (req,res) => {
+    try{
+        let id = req.params.id;
+        let updateUser = req.body;
+        const results = await conn.query(
+            'UPDATE users SET ? WHERE id = ?', 
+            [updateUser, id]
+        )
+        res.json({
+            message: 'Update User Completed',
+            data: results[0]
+        })
+    } catch (error){
+        console.error('errorMessage', error.message)
+        res.status(500).json({
+            message: "Something went wrong",
+            errorMessage: error.message
+        })
+    }
+})
+
+// Path = GET /users/:id
+app.get('/users/:id',async (req,res) => {
+    try{
+    let id = req.params.id;
+    const results = await conn.query('SELECT * FROM users WHERE id = ?', id)
+    if(results[0].length == 0){
+        throw {statusCode: 404, message: 'User not found'}
+    }
+    res.json(results[0][0])
+} catch (error){
+    console.error('errorMessage', error.message)
+    let statusCode = error.statusCode || 500
+        res.status(statusCode).json({
+            message: "Something went wrong",
+            errorMessage: error.message
+        })
+    }
+});
 
 // Path = DELETE / user/:id
-app.delete('/user/:id', (req,res) => {
-    let id = req.params.id;
-    // find index of user
-    let selectedIndex = users.findIndex(user => user.id == id)
-    
-    users.splice(selectedIndex,1)
-    delete users[selectedIndex]
-    res.json({
-        message: 'Delete Completed',
-        indexDelete: selectedIndex
-    });
+app.delete('/users/:id', async(req,res) => {
+    try{
+        let id = req.params.id;
+        const results = await conn.query('DELETE from users WHERE id = ?', id)
+        res.json({
+            message: 'Delete User Completed',
+            data: results[0]
+        })
+    } catch (error){
+        console.error('errorMessage', error.message)
+        res.status(500).json({
+            message: "Something went wrong",
+            errorMessage: error.message
+        })
+    }
 });
 
 app.listen(port, async () => {
